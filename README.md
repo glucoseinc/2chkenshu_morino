6f9ecd8d3840b520234ed82072db418b22f7549c
shallow90125 <shallow90125@gmail.com>
2025-08-19 16:28:39 +0900

    feat: install tailwindcss

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ npm create vite@latest declarative-react-router -- --template react-ts
 # https://reactrouter.com/start/declarative/installation
 npm install react-router
 ```
+
+```bash
+# https://tailwindcss.com/docs/installation/using-vite
+npm install tailwindcss @tailwindcss/vite
+
+```
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,20 @@ export const Index: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <button onClick={() => setCount((count) => count - 1)}>-1</button>
-      <span>{count}</span>
-      <button onClick={() => setCount((count) => count + 1)}>+1</button>
+    <div className="h-dvh flex place-items-center place-content-center">
+      <button
+        className="border w-8"
+        onClick={() => setCount((count) => count - 1)}
+      >
+        -1
+      </button>
+      <span className="w-8 grid place-items-center">{count}</span>
+      <button
+        className="border w-8"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        +1
+      </button>
     </div>
   );
 };
